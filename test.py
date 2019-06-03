f114c1a43a94d1e59ee8996b5a6435717eaf11b8
Zhi-Weng Chua <Zhi-Weng.Chua@bom.gov.au>
2019-06-03 16:24:30 +1000

    initialise

---
+++ test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from netCDF4 import Dataset
+import urllib
+
+url='ftp://semdp:SEMaP+1803@hokusai.eorc.jaxa.jp/GSMaP_GNRT/DATA/2019/201905/SEMDP_GSMaP_GNRT6_0.10deg-MON_201905.nc'
+
+filename=url[-39:]
+print(filename)
+
+urllib.request.urlretrieve(url, filename)
+
+dataset=Dataset(filename)
+
+#print(dataset.dimensions.keys())
